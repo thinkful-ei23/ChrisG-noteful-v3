01619676ae5665e13dd0f72a70e6f08b676b927d
Chris Gantz <chrisgantz836@gmail.com>
2018-08-20 15:47:57 -0700

    all good for users impl

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,82 @@ const router = express.Router();
 
 router.post('/', (req, res, next) => {
   const { fullname, username, password } = req.body;
+  const requiredFields = ['username', 'password'];
+  const missingField = requiredFields.find(field => !(field in req.body));
 
-  return User.hashPassword(password)
+  if (missingField) {
+    const err = new Error(`Missing ${missingField} in request body`);
+    err.status = 422;
+    return next(err);
+  }
+  const stringFields = ['username', 'password', 'firstName', 'lastName'];
+  const nonStringField = stringFields.find(
+    field => field in req.body && typeof req.body[field] !== 'string'
+  );
+
+  if (nonStringField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Incorrect field type: expected string',
+      location: nonStringField
+    });
+  }
+
+  const explicityTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicityTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+
+  if(nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Cannot start or end with whitespace',
+      location: nonTrimmedField
+    });
+  }
+
+  const minMaxSize = {
+    username: {
+      min: 1
+    },
+    password: {
+      min: 8,
+      max: 72
+    }
+  };
+
+  const tooSmallField = Object.keys(minMaxSize).find(
+    field => 'min' in minMaxSize[field] && req.body[field].trim().length < minMaxSize[field].min
+  );
+  const tooLargeField = Object.keys(minMaxSize).find(
+    field => 'max' in minMaxSize[field] && req.body[field].trim().length > minMaxSize[field].max
+  );
+
+  if(tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField ? `Must be at least ${minMaxSize[tooSmallField].min} characters long` : `Must be at most ${minMaxSize[tooLargeField].max} characters long`,
+      location: tooSmallField || tooLargeField
+    });
+  }
+
+  // check for existing user
+  return User.find({ username })
+    .count()
+    .then(count => {
+      if (count > 0) {
+        return Promise.reject({
+          code: 422,
+          reason: 'ValidationError',
+          message: 'Username already taken',
+          location: 'username'
+        });
+      }
+      return User.hashPassword(password);
+    })
     .then(digest => {
       const newUser = {
         username,
@@ -24,13 +98,11 @@ router.post('/', (req, res, next) => {
       res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
     })
     .catch(err => {
-      if (err.code === 11000) {
-        err = new Error('This username already exists');
-        err.status = 400;
+      if (err.reason === 'ValidationError') {
+        return res.status(err.code).json(err);
       }
-      next(err);
+      res.status(500).json({ code: 500, message: 'Internal server error' });
     });
-
 });
 
 module.exports = router;
